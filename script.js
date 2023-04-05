5ac2401d0ed7cb607ebfda84ef70ded8fc7419ec
shawn0rion <shawnorion07@gmail.com>
2023-04-05 14:48:52 -0500

    keys can be clicked

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 const allScales = [];
+let userScale = [];
+let possibleScales = [];
 
 const whiteKeys = document.querySelectorAll(".white-key");
+console.log(whiteKeys);
 const blackKeys = document.querySelectorAll(".black-key");
+const keys = [...whiteKeys].concat([...blackKeys]);
+console.log(keys);
+keys.forEach((key) => {
+  console.log(key);
+  key.addEventListener("click", (e) => {
+    console.log(key);
+    toggleActive(e);
+  });
+});
 
 function createObject(title, scale) {
   return {
@@ -27,7 +39,6 @@ function allNotesFromRoot(root) {
 function createScale(root, isMajor) {
   // redefine all notes, beginning from the root
   const newNotes = allNotesFromRoot(root);
-  console.log(newNotes);
 
   // determine major or minor scale formula
   let formula = [];
@@ -36,8 +47,6 @@ function createScale(root, isMajor) {
   } else {
     formula = [0, 2, 3, 5, 7, 8, 10];
   }
-  console.log(formula);
-
   const filtered = [];
   let formulaIdx = 0;
   // filter notes and add any note that matches the formula
@@ -64,3 +73,24 @@ function generateScales() {
 }
 
 console.log(generateScales());
+
+function toggleActive(event) {
+  const { target } = event;
+  const { textContent } = target;
+
+  if (target.classList.contains("active")) {
+    target.classList.remove("active");
+
+    const idx = userScale.indexOf(textContent);
+    userScale = [...userScale.slice(0, idx)];
+    // if the last note is not selected, append all notes after the selected note
+    if (idx !== userScale.length - 1) {
+      userScale.concat([...userScale.slice(idx + 1)]);
+    }
+  } else {
+    target.classList.add("active");
+    userScale.push(textContent);
+  }
+  console.log(target);
+  console.log(userScale);
+}
